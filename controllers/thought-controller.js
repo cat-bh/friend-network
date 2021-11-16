b4d75f9c4b75519021735be038f7a2792aefec5a
Catherine <catheneghan@gmail.com>
2021-11-16 00:26:45 -0800

    Reaction add and remove routes working

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -73,6 +73,38 @@ const thoughtController = {
                 res.json(dbThoughtData);
             })
             .catch(err => res.status(400).json(err));
+    },
+    // Add Reaction
+    addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $push: { reactions: body }},
+            { new: true, runValidators: true }
+        )
+            .then(dbThoughtData => {
+                if(!dbThoughtData) {
+                    res.status(404).json({message: "No thought found with this id"});
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.status(500).json(err));
+    },
+    // Remove reaction
+    removeReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reactions: { reactionId: params.reactionId }}},
+            { new: true }
+        )
+            .then(dbThoughtData => {
+                if(!dbThoughtData) {
+                    res.status(404).json({message: "No Thought found with this id"});
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.status(500).json(err));
     }
 };
 
