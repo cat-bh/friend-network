a88cd07e4c2b8ba959e0afee5dbc19cc474b5e5b
Catherine <catheneghan@gmail.com>
2021-11-16 00:08:18 -0800

    add and remove friend routes working

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -9,6 +9,10 @@ const userController = {
                 model: 'Thought',
                 select: '-__v'
             })
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
             .select('-__v')
             .then(dbUserData => res.json(dbUserData))
             .catch(err => {
@@ -69,6 +73,41 @@ const userController = {
                 res.json(dbResponse);
             })
             .catch(err => res.status(400).json(err));
+    },
+    // Add Friend
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $push: { friends: params.friendId }},
+            { new: true }
+        )
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({message: "No User found with this id"});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+    // Remove friend
+    removeFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId}},
+            { new: true }
+        )
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({message: "No user found with this id"});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(500).json(err));
     }
 };
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
 const checkEmail = function(email) {
     const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
